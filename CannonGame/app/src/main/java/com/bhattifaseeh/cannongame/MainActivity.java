3a663a91f890d37f5ed3d63e8d6ffdddb4497b50
faseehbhatti <55502131+faseehbhatti@users.noreply.github.com>
2022-05-23 13:46:10 -0400

    Add files via upload

---
+++ CannonGame/app/src/main/java/com/bhattifaseeh/cannongame/MainActivity.java
@@ -0,0 +1,15 @@
+package com.bhattifaseeh.cannongame;
+
+//   “On my honor, I have neither received nor given any unauthorized assistance on Mid Term Exam. Faseeh Bhatti"
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    // called when the app first launches
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
